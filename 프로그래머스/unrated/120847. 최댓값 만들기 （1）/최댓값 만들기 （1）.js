3e4a09801468273e18bf7d8d01d11076154b345c
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-12 21:49:00 +0900

    [level unrated] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,12 +1,4 @@
 function solution(numbers) {
-    const maxValue = Math.max(...numbers);
-    let arr = numbers.filter((num) => num === maxValue);
-    if(arr.length > 1) return maxValue * maxValue
-    
-    arr = numbers.filter((num) => num !== maxValue);
-    const maxValue2 = Math.max(...arr);
-    
-    
-    
-    return maxValue * maxValue2;
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
 }
