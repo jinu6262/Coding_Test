59f47b9f5620eb4ee6b9fed5112eb6d095699e28
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-13 17:45:13 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,8 +1,6 @@
 function solution(my_string) {
-    let my_arr = my_string.split('')
-    const arr = my_arr.filter(c => Number(c))
     let sum = 0
-    arr.map(n => sum = sum + Number(n))
+    const arr = [...my_string].filter(c => Number(c)).map(n => sum = sum + Number(n))
     
     return sum;
 }
