e5090f3b35647deefa5cf0084d2bb11832565055
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-14 17:15:29 +0900

    [level unrated] Title: 암호 해독, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,8 @@
 function solution(cipher, code) {
-    const cipher_arr = cipher.split("")
     let new_str = ''
     
-    for(let i = 0; i<cipher_arr.length; i++){
-        if((i+1)%code === 0) new_str = new_str + cipher_arr[i]
+    for(let i = 0; i<cipher.length; i++){
+        if((i+1)%code === 0) new_str = new_str + cipher[i]
     }
     
     return new_str;
