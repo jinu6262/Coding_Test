c1df5cda9fdde0de82b346a5b946b86646caf092
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-14 17:48:07 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,12 +1,12 @@
 function solution(numbers) {
-    let max = numbers[0]*numbers[1]
-    for(let i=0; i<numbers.length; i++){
-        for(let j=i+1; j<numbers.length; j++){
-            const value = numbers[i]*numbers[j]
-            if(max < value){
-                max = value
-            }
-        }
-    }
-    return max;
+    const N = numbers.length;
+
+    numbers.sort((a,b) => a - b);
+
+    return Math.max(
+        numbers[N-1] * numbers[N-2],
+        numbers[0] * numbers[1]
+    )
 }
+
+// 가장 큰 값, 작은 값 구할 때 - 오름차순
