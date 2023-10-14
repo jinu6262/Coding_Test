99d467e95fdaac4e07ee6e617f4325233f6142dd
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-10-14 16:13:04 +0900

    [level unrated] Title: 개미 군단, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -3,9 +3,9 @@ function solution(hp) {
     let now_hp = hp;
     const ants = [5,3,1]
     
-    for(i=0; i<ants.length; i++){
-        count = count + parseInt(now_hp/ants[i])
-        now_hp = now_hp%ants[i]
+    for(const ant of ants){
+        count = count + parseInt(now_hp/ant)
+        now_hp = now_hp%ant
     }
     
     return count;
