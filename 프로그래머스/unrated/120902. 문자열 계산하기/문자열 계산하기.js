d3d49ec2351ba04adca76d07ed18144176a9c9d6
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-11-06 22:02:44 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let arr = my_string.split(" ")
+    let result = Number(arr[0])
+    
+    for(let i = 1; i<arr.length; i = i + 2){
+        if(arr[i] === '+') result += Number(arr[i+1])
+        if(arr[i] === '-') result -= Number(arr[i+1])
+    }
+    
+    return result
+}
