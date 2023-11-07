419da874de6018d9d67fd6d5df24314ca67c340b
Kim Jinu <105920346+jinu6262@users.noreply.github.com>
2023-11-07 23:25:13 +0900

    [level unrated] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,21 +1,11 @@
 function solution(dots) {    
-    const point1 = dots[0][0]
-    const point2 = dots[0][1]
-    let max_x = 0
-    let min_x = 0
-    let max_y = 0
-    let min_y = 0
-    
-    for(let i = 0; i<dots.length; i++){
-        dots[i][0] -= point1
-        dots[i][1] -= point2
-        
-        max_x = Math.max(dots[i][0], max_x)
-        min_x = Math.min(dots[i][0], min_x)
-        
-        max_y = Math.max(dots[i][1], max_y)
-        min_y = Math.min(dots[i][1], min_y)
+    let x = [],
+        y = [];
+
+    for (let pos of dots) {
+        x.push(pos[0]);
+        y.push(pos[1]);
     }
-    
-    return Math.abs(max_x - min_x) * Math.abs(max_y - min_y)
+
+    return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y))
 }
